f3985380715bc2a9688e4066a0351c7870e9e7d1
GulsenZalova <zalova0202@gmail.com>
2022-03-14 01:51:10 +0400

    Javascripts conditions (if, else, else if part1)

---
+++ Javascript/conditions/conditions.js
@@ -0,0 +1,15 @@
+let sira="1";
+
+if(sira=="1"){
+    console.log("Zəhmət olmasa 1 nömrəli zala daxil olun")
+}else if(sira=="2"){
+    console.log("Zəhmət olmasa 2 nömrəli zala daxil olun")
+}else if(sira=="3"){
+    console.log("Zəhmət olmasa 3 nömrəli zala daxil olun")
+}else if(sira=="4"){
+    console.log("Zəhmət olmasa 4 nömrəli zala daxil olun")
+}else if(sira=="5"){
+    console.log("Zəhmət olmasa 5 nömrəli zala daxil olun")
+}else{
+    console.log("Zəhmət olmasa bir az gözləyin")
+}
