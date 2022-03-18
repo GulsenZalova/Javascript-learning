8dc7246f93ace557636d776a5ead74563555ff1a
GulsenZalova <zalova0202@gmail.com>
2022-03-19 03:09:16 +0400

    array practice

--- Javascript/array/array_2.js
+++ Javascript/array/array_2.js
@@ -1,8 +1,8 @@
 // Aşağıdakı proqram arraydəki məlumatları cümlə şəklində ekrana yazdırmaq üçün yazılıb.
-var now=2018;
-var models=['Toyota','Mazda','Mercedes','Opel'];
-var years=['2015','2012','2016','2014'];
+// var now=2018;
+// var models=['Toyota','Mazda','Mercedes','Opel'];
+// var years=['2015','2012','2016','2014'];
 
-for (let i=0; i<models.length; i++){
-    console.log(models[i] + ' '+ years[i] + ' model və ' + (now -years[i] )+ ' yaşdında bir maşındır ' )
-}
+// for (let i=0; i<models.length; i++){
+//     console.log(models[i] + ' '+ years[i] + ' model və ' + (now -years[i] )+ ' yaşdında bir maşındır ' )
+// }
