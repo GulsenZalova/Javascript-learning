755b7d0b773885aab68dd63e7db8da8b94104d66
GulsenZalova <zalova0202@gmail.com>
2022-03-19 01:57:25 +0400

    array practise

---
+++ Javascript/array/array_1.js
@@ -0,0 +1,8 @@
+// Aşağıdakı proqram arrayin bütün elementlərini ekranda yazdırmaq üçün yazılmışdır.
+var now=2018;
+var models=['Toyota','Mazda','Mercedes','Opel'];
+var years=['2015','2012','2016','2014'];
+
+alert(models);
+alert(now);
+alert(years)
---
+++ Javascript/array/array_2.js
@@ -0,0 +1,5 @@
+// Aşağıdakı proqram arraydəki məlumatları cümlə şəklində ekrana yazdırmaq üçün yazılıb.
+var now=2018;
+var models=['Toyota','Mazda','Mercedes','Opel'];
+var years=['2015','2012','2016','2014'];
+
