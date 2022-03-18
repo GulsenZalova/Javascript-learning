bf8efd1147a2b5f45a02d9fba1268fcea7c326e1
GulsenZalova <zalova0202@gmail.com>
2022-03-18 22:06:53 +0400

    js conditions(switch)

--- Javascript/conditions/conditions_1.js
+++ Javascript/conditions/conditions_1.js
@@ -1,17 +1,17 @@
 // Aşağıdakı proqram ziyarətçinin sıra nömrəsinə əsasən daxil olacağı zalın müəyyənləşdirilməsi üçün yazılıb.(if, else if və else ilə)
 
-// let sira="1";
+let sira="1";
 
-// if(sira=="1"){
-//     console.log("Zəhmət olmasa 1 nömrəli zala daxil olun")
-// }else if(sira=="2"){
-//     console.log("Zəhmət olmasa 2 nömrəli zala daxil olun")
-// }else if(sira=="3"){
-//     console.log("Zəhmət olmasa 3 nömrəli zala daxil olun")
-// }else if(sira=="4"){
-//     console.log("Zəhmət olmasa 4 nömrəli zala daxil olun")
-// }else if(sira=="5"){
-//     console.log("Zəhmət olmasa 5 nömrəli zala daxil olun")
-// }else{
-//     console.log("Zəhmət olmasa bir az gözləyin")
-// }
+if(sira=="1"){
+    console.log("Zəhmət olmasa 1 nömrəli zala daxil olun")
+}else if(sira=="2"){
+    console.log("Zəhmət olmasa 2 nömrəli zala daxil olun")
+}else if(sira=="3"){
+    console.log("Zəhmət olmasa 3 nömrəli zala daxil olun")
+}else if(sira=="4"){
+    console.log("Zəhmət olmasa 4 nömrəli zala daxil olun")
+}else if(sira=="5"){
+    console.log("Zəhmət olmasa 5 nömrəli zala daxil olun")
+}else{
+    console.log("Zəhmət olmasa bir az gözləyin")
+}
---
+++ Javascript/conditions/conditions_6.js
@@ -0,0 +1,28 @@
+// Aşağıdakı proqram istifadəçi tərəfindən daxil edilən rəqəmə əsasən ilin fəslinin müəyyənləşdirilməsi üçün yazılmışdır.(switch ilə)
+
+let fesil=prompt("Zəhmət olmasa dəyər daxil edin: ");
+
+switch(fesil){
+    case '12':
+    case '1':
+    case '2':
+        alert(" Hal-hazırda qış fəslidir. ");
+        break;
+    case '3':
+    case '4':
+    case '6':
+        alert(" Hal-hazırda yaz fəslidir. ");
+        break;
+    case '7':
+    case '8':
+    case '9':
+        alert(" Hal-hazırda yay fəslidir. ");
+        break;
+    case '10':
+    case '11':
+    case '12':
+        alert(" Hal-hazırda payız fəslidir. ");
+        break;
+    default:
+    alert("Zəhmət olmasa düzgün dəyər daxil etdiyinizdən əmin olun !!!")
+}
