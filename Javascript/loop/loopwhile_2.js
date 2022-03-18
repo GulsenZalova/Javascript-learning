69dcd8694a88f1de9318b15c94f994aaccb0ce6c
GulsenZalova <zalova0202@gmail.com>
2022-03-19 00:07:11 +0400

    foor while task2

---
+++ Javascript/loop/loopwhile_2.js
@@ -0,0 +1,9 @@
+// Aşağıdakı proqram 1-dən 10-a qədər sadəcə cüt ədədləri ekrana yazdırmaq üçün yazılıb. ( while ilə)
+
+let i=0;
+while(i<=10){
+    i++;
+    if(i%2==0){
+        console.log(i);
+    }
+}
