2c95283bba93e595be06837ec1f6204a9992e696
GulsenZalova <zalova0202@gmail.com>
2022-03-18 23:42:57 +0400

    loop while task1

--- Javascript/loop/loopfor_1.js
+++ Javascript/loop/loopfor_1.js
@@ -1,7 +1,7 @@
 // Aşağıdakı proqram 1dən 10-a qədər ədədləri ekrana yazdırmaq üçün yazılıb. ( For ilə)
 
-for(let i=0; i<=10; i++){
-    alert(i)
-    console.log(i)
-}
+// for(let i=0; i<=10; i++){
+//     alert(i)
+//     console.log(i)
+// }
 
---
+++ Javascript/loop/loopwhile_1.js
@@ -0,0 +1,7 @@
+// Aşağıdakı proqram 1dən 10-a qədər ədədləri ekrana yazdırmaq üçün yazılıb. ( While ilə)
+
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
