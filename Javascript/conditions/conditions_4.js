5a7d1648af65d851a2d94550ab3b90d45630b35e
GulsenZalova <zalova0202@gmail.com>
2022-03-18 22:35:54 +0400

    js conditions il hesablama with if else else if)

--- Javascript/conditions/conditions_4.js
+++ Javascript/conditions/conditions_4.js
@@ -1,32 +1,32 @@
 /* Aşağıdakı proqram tələbənin daxil etdiyi məlumatlara əsasən onun imtahanda hansı nəticəni göstərdiyini müəyyənləşdirmək üçün yazılıb.(swtich ilə)*/
 
-let ad=prompt("Zəhmət olmasa adınızı daxil edin: ");
-let soyad=prompt("Zəhmət olmasa soyadınızı daxil edin: ");
-let fənn=prompt("Zəhmət olmasa fənn adını daxil edin: ");
-let imtahanBali=prompt("Zəhmət olmasa imtahan balınızı daxil edin: ");
+// let ad=prompt("Zəhmət olmasa adınızı daxil edin: ");
+// let soyad=prompt("Zəhmət olmasa soyadınızı daxil edin: ");
+// let fənn=prompt("Zəhmət olmasa fənn adını daxil edin: ");
+// let imtahanBali=prompt("Zəhmət olmasa imtahan balınızı daxil edin: ");
 
-switch (imtahanBali){
-    case '50':
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə F-dir.");
-        break;
-    case '60':
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə E-dir.");
-        break;
-    case '70' :
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə D-dir.");
-        break;
-    case '80':
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə C-dir.");
-        break;
-    case '90':
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə B-dir.");
-        break; 
-    case '100':
-        alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə A-dir.");
-        break;
-    default:
-        alert("Zəhmət olmasa daxil etdiyiniz imtahan nəticəsinin düzgünlüyünü bir daha yoxlayın!!!")
-}  
+// switch (imtahanBali){
+//     case '50':
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə F-dir.");
+//         break;
+//     case '60':
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə E-dir.");
+//         break;
+//     case '70' :
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə D-dir.");
+//         break;
+//     case '80':
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə C-dir.");
+//         break;
+//     case '90':
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə B-dir.");
+//         break; 
+//     case '100':
+//         alert(ad + " " + soyad + " " + "sizin" + " " + fənn + " " + "fənnindən əldə etdiyiniz nəticə A-dir.");
+//         break;
+//     default:
+//         alert("Zəhmət olmasa daxil etdiyiniz imtahan nəticəsinin düzgünlüyünü bir daha yoxlayın!!!")
+// }  
 // İmtahan balı üçün switch-də aralıq yaza bilmədim, yenidən araşdır
 
      
---
+++ Javascript/conditions/conditions_7.js
@@ -0,0 +1,11 @@
+// Aşağıdakı proqram istifadəçi tərəfin daxil edilən doğum ilinə əsasən ona sürücülük vəsiqəsi verilib-verilməyəcəyini müəyyənləşdirmək üçün yazılmışdır(if, else if və else ilə)
+
+let il=prompt("Zəhmət olmasa doğum ilinizi  daxil edin: ")
+let ferq= 2022-il;
+let qalıq= 18 - ferq;
+
+if(ferq>=18){
+    alert("Sizin yaşınız sürücülük vəsiqəsi almaq üçün kifəyətdir.")
+}else if(ferq<18){
+    alert(`Sizin yaşınız sürücülük vəsiqəsi almaq üçün kifayət deyildir. Daha ${qalıq} il gözləməlisiniz.`)
+}
