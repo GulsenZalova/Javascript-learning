75bb2335aa37f0c9d59e3f781e13adca95c0ac6c
GulsenZalova <zalova0202@gmail.com>
2022-03-19 17:21:36 +0400

    jacascript task_3

---
+++ Javascript/function/function_3.js
@@ -0,0 +1,31 @@
+// Aşağıdakı proqram istifadəçi tərəfindən daxil edilən ədədin tək yoxsa çüt olmasını müəyyənləşdirmək üçün yazılmışdır.
+
+// birinci üsul
+let num=prompt("Zəhmət olmasa ədəd daxil edin.");
+
+function bolmə(num){
+    return num%2
+}
+let hesabla=bolmə(num)
+console.log(hesabla)
+
+if((hesabla>0)&&(hesabla==0)){
+    alert(num + " cüt" + " ədəddir.")
+}else if((hesabla>0)&&(hesabla!=0)){
+    alert(num + " tək" + " ədəddir.")
+}else{
+    alert("Zəhmət olmasa düzgün dəyər daxil etdiyinizdən əmin olun.")
+}
+
+// ikinci üsul
+let eded=prompt("Zəhmət olmasa ədəd daxil edin.");
+
+function tekCut(eded){
+    if(num%2==0){
+        return eded+ " cüt" + " ədəddir."
+    }else{
+        return eded+ " tək" + " ədəddir."
+    }
+}
+
+console.log(tekCut(eded))
