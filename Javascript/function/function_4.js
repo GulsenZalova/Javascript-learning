2defcf3234dc53ecd81bce9f6341e3f4537989ac
GulsenZalova <zalova0202@gmail.com>
2022-03-20 02:46:18 +0400

    js function task_5

--- Javascript/function/function_4.js
+++ Javascript/function/function_4.js
@@ -1,12 +1,12 @@
 // Aşağıdakı proqram daxil edilən ədədin faktorialını tapmaq üçün yazılmışdır.
 
-let num=prompt("Zəhmət olmasa ədəd daxil edin. ");
+// let num=prompt("Zəhmət olmasa ədəd daxil edin. ");
 
-function faktorialHesabla (num){
-    for(let i=1; i<num ;i-- ){
-        num*=i;
-    }
-    return num;
-}
-let faktorial=faktorialHesabla(num);
-console.log(faktorial)
+// function faktorialHesabla (num){
+//     for(let i=1; i<num ;i-- ){
+//         num*=i;
+//     }
+//     return num;
+// }
+// let faktorial=faktorialHesabla(num);
+// alert(faktorial)
---
+++ Javascript/function/function_5.js
@@ -0,0 +1,6 @@
+// AŞağıdakı proqram yas hesablanması məqsədilə yazılmışdır.
+
+function yasHesabla(year,now){
+    return (now-year);
+}
+ alert(yasHesabla(1997,2022))
