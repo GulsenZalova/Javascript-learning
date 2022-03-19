026cf441abb64ef7e0cca6d4b1cf05b88f466e44
GulsenZalova <zalova0202@gmail.com>
2022-03-19 16:45:55 +0400

    function task_2

--- Javascript/function/function_1.js
+++ Javascript/function/function_1.js
@@ -1,26 +1,26 @@
 // Funksiya vasitəsilə hesablama əməlləri
-function topla(a,b){
-    return a+b;
-}
-function cix(a,b){
-    return a-b;
-}
-function vur(a,b){
-    return a*b;
-}
-function böl(a,b){
-    return a/b;
-}
+// function topla(a,b){
+//     return a+b;
+// }
+// function cix(a,b){
+//     return a-b;
+// }
+// function vur(a,b){
+//     return a*b;
+// }
+// function böl(a,b){
+//     return a/b;
+// }
 
-let hesabla=topla(5,10);
-    console.log(hesabla)
+// let hesabla=topla(5,10);
+//     console.log(hesabla)
 
-hesabla=cix (5,10);
-    console.log(hesabla)
+// hesabla=cix (5,10);
+//     console.log(hesabla)
 
-hesabla=vur(5,10);
-    console.log(hesabla)
+// hesabla=vur(5,10);
+//     console.log(hesabla)
 
-hesabla=böl(5,10);
-    console.log(hesabla)
+// hesabla=böl(5,10);
+//     console.log(hesabla)
 
---
+++ Javascript/function/function_2.js
@@ -0,0 +1,18 @@
+// Aşağıdakı proqram bir şəxsin pensiya yaşına çatıb çatmadığını müəyyənləşdirmək üçün hesablama aparmaq məqsədilə yazılmışdır.
+
+let yas=prompt("Zəhmət olmasa doğum ilinizi daxil edin.");
+let now=2022
+
+function pensiyaHesabla(yas,now){
+    return now-Number(yas)
+}
+let hesabla=pensiyaHesabla(yas,now)
+console.log(hesabla)
+
+if ((hesabla>65)&&(hesabla<150)){
+    alert(  hesabla  + ' yaş'+ ' müəyyənləşdirilmiş pensiya yaşına uyğundur.')
+}else if ((hesabla>0)&&(hesabla<65)){
+    alert(hesabla + " yas" + " müəyyənləşdirilmiş pensiya yaşına uyğun deyildir." + " Daha" + " " + (65-hesabla)+ " il vaxt var.")
+}else{
+    alert("Zəhmət olmasa düzgün dəyər daxil edin")
+}
