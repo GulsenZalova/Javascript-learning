ae5e7bd45d66af739c0a8a312ecce1bfac9596d6
GulsenZalova <zalova0202@gmail.com>
2022-03-19 16:07:20 +0400

    function task_1

--- Javascript/array/array_3.js
+++ Javascript/array/array_3.js
@@ -1,14 +1,14 @@
 // Aşağıdakı proqram array daxilinə element əlavə etmək üçün yazılıb.
-var now=2022;
-var models=['Toyota','Mazda','Mercedes','Opel'];
-var years=['2015','2012','2016','2014'];
+// var now=2022;
+// var models=['Toyota','Mazda','Mercedes','Opel'];
+// var years=['2015','2012','2016','2014'];
 
-let model=prompt("Zəhmət olmasa model daxil edin: ");
-let year=prompt("Zəhmət olmasa il daxil edin");
+// let model=prompt("Zəhmət olmasa model daxil edin: ");
+// let year=prompt("Zəhmət olmasa il daxil edin");
 
-models.push(model);
-years.push(year);
+// models.push(model);
+// years.push(year);
 
-for (let i=0; i<models.length; i++){
-    console.log(models[i]+' '+ years[i]+" model və "+ (now-years[i]) + " yaşında bir maşındır")
-}
+// for (let i=0; i<models.length; i++){
+//     console.log(models[i]+' '+ years[i]+" model və "+ (now-years[i]) + " yaşında bir maşındır")
+// }
