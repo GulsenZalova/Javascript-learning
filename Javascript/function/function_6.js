0a1b536af7822b283f2919651a7771679488b275
GulsenZalova <zalova0202@gmail.com>
2022-03-20 02:58:22 +0400

    js function task_6

---
+++ Javascript/function/function_6.js
@@ -0,0 +1,8 @@
+// Aşağıdakı proqram mesajın kimdən gəldiyini və mesajın məzmununu göstərmək üçün yazılmışdır.
+
+function message(name,content){
+    return (name + ' :'+ content)
+}
+
+alert(message("Gülşən"," Salam necəsən?"));
+alert(message("Aytən", " Salam"));
