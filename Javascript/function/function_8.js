d11a656f5d29ee93ef612e90976dfc826cea4da2
GulsenZalova <zalova0202@gmail.com>
2022-03-21 02:07:48 +0400

    function task_9

--- Javascript/function/function_8.js
+++ Javascript/function/function_8.js
@@ -1,16 +1,16 @@
 // Aşağıdakı proqram ədədin qüvvətini tapmaq üçün yazılmışdır.
 
-let x=prompt("Zəhmət olmasa ədəd daxil edin: ");
-let n=prompt("Zəhmət olmasa ədəd daxil edin: ");
+// let x=prompt("Zəhmət olmasa ədəd daxil edin: ");
+// let n=prompt("Zəhmət olmasa ədəd daxil edin: ");
 
-function usAl(x,n){
-    for(let i=1; i<n; i++){
-    return (Math.pow(x,n));
-    }
-}
+// function usAl(x,n){
+//     for(let i=1; i<n; i++){
+//     return (Math.pow(x,n));
+//     }
+// }
 
-if(n<1){
- alert(`${n}`+ "'ədədi natural ədəd deyildir")
-}else{
- alert(usAl(x,n))   
-}
+// if(n<1){
+//  alert(`${n}`+ "'ədədi natural ədəd deyildir")
+// }else{
+//  alert(usAl(x,n))   
+// }
---
+++ Javascript/function/function_9.js
@@ -0,0 +1,9 @@
+// Aşağıdakı proqram array elementlərini ekrana yazdırmaq üçün yazılıb
+var models=['Toyota','Mazda','Mercedes','Opel'];
+
+function show(){
+    for(let i=0; i<models.length; i++){
+        console.log(models[i])
+    }
+}
+show()
