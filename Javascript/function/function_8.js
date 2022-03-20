81883ade42bcd628f189ff6153787d4234e63a27
GulsenZalova <zalova0202@gmail.com>
2022-03-20 16:27:18 +0400

    function task_8

--- Javascript/function/function_8.js
+++ Javascript/function/function_8.js
@@ -1,9 +1,16 @@
 // Aşağıdakı proqram ədədin qüvvətini tapmaq üçün yazılmışdır.
 
+let x=prompt("Zəhmət olmasa ədəd daxil edin: ");
+let n=prompt("Zəhmət olmasa ədəd daxil edin: ");
+
 function usAl(x,n){
+    for(let i=1; i<n; i++){
     return (Math.pow(x,n));
+    }
 }
 
-alert(usAl(3,2));
-alert(usAl(3,3));
-alert(usAl(1,100));
+if(n<1){
+ alert(`${n}`+ "'ədədi natural ədəd deyildir")
+}else{
+ alert(usAl(x,n))   
+}
