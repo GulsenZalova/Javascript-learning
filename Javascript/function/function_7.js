0fbfe042b20ffa806b54eb83a39bde4f70f66f76
GulsenZalova <zalova0202@gmail.com>
2022-03-20 04:16:23 +0400

    js function task_8

--- Javascript/function/function_7.js
+++ Javascript/function/function_7.js
@@ -1,15 +1,15 @@
 // Aşağıdakı kod daxil edilən arqumentə görən uyğun dəyəri qaytarmaq məqsədilə yazılmışdır.
 
-function min(a,b){
-    if(a>b){
-        return (a + " böyükdür")
-    }else if(b>a){
-        return (b + " böyükdür")
-    }else if(b==a){
-        return ('Ədədlər' + " bərabərdir")
-    }
-}
+// function min(a,b){
+//     if(a>b){
+//         return (a + " böyükdür")
+//     }else if(b>a){
+//         return (b + " böyükdür")
+//     }else if(b==a){
+//         return ('Ədədlər' + " bərabərdir")
+//     }
+// }
 
-alert(min(2,5));
-alert(min(3,-1));
-alert(min(1,1));
+// alert(min(2,5));
+// alert(min(3,-1));
+// alert(min(1,1));
---
+++ Javascript/function/function_8.js
@@ -0,0 +1,9 @@
+// Aşağıdakı proqram ədədin qüvvətini tapmaq üçün yazılmışdır.
+
+function usAl(x,n){
+    return (Math.pow(x,n));
+}
+
+alert(usAl(3,2));
+alert(usAl(3,3));
+alert(usAl(1,100));
