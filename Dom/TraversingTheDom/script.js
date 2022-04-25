1371a9f6023ebc3d3531934f6015a77e036ac0bc
GulsenZalova <zalova0202@gmail.com>
2022-04-25 04:46:06 +0400

    dom - traversing the dom

---
+++ Dom/TraversingTheDom/script.js
@@ -0,0 +1,12 @@
+// let val;
+// let list=document.querySelector(".list-group")
+// val=list.childNodes[0].nodeName
+// val=list.childNodes[0].nodeType
+// val=list.childNodes[1].nodeType
+// console.log(val)
+
+let val;
+let list=document.querySelector(".list-group")
+val=list.children[0].style.color='red'
+val.list.children[1].textContent='salam'
+console.log(val)
