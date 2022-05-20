3d245d1a8f66b2dc16db0d962ec9479251962faa
GulsenZalova <zalova0202@gmail.com>
2022-05-21 02:21:50 +0400

    http Get&&Post

--- Javascipt API/HTTpGet/script.js
+++ Javascipt API/HTTpGet/script.js
@@ -1,5 +1,7 @@
 document.querySelector("#getOne").addEventListener("click",getOne)
 document.querySelector("#getAll").addEventListener("click",getAll)
+document.querySelector("#postData").addEventListener("click",postData)
+
 
 function getAll(){
 var loadimage=document.querySelector("#loading")
@@ -70,3 +72,31 @@ function getOne(){
     
     }, 1500)
 }
+
+
+// Yen bir məlumatın bazaya yazılması
+function postData(){
+   const data={
+       userId: 1,
+       title: "new title",
+       body:"new body",
+   }
+    const json=JSON.stringify(data)
+    // console.log(post)
+    // console.log(data)
+    const url = "https://jsonplaceholder.typicode.com/posts/"
+    const xhr= new XMLHttpRequest()
+    xhr.open("POST", url , true)
+    xhr.setRequestHeader("Content-type","application/json;charset=utf-8")
+
+    xhr.onload=function(){
+        if(xhr.readyState===4 && xhr.status===201){
+            const post=xhr.responseText
+            console.log(post)
+        }
+    }
+
+    xhr.send(json)
+}
+
+
