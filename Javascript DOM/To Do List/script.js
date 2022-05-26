8397050f165f5686b1ba0df01b77078c5aef5f0a
GulsenZalova <zalova0202@gmail.com>
2022-05-26 23:47:33 +0400

    update

--- Javascript DOM/To Do List/script.js
+++ Javascript DOM/To Do List/script.js
@@ -1,44 +1,19 @@
 const newTask=document.querySelector(".input-task");
 const newTaskAddBtn=document.querySelector(".btn-task-add");
 const taskList=document.querySelector(".task-list");
-
-
+document.addEventListener("DOMContentLoaded",localStorageOxu)
 newTaskAddBtn.addEventListener("click", taskAdd);
-function taskAdd(e){
-    if(newTask.value==''){
-        alert("Task Əlavə edin!!!")
-    }else{
-        let taskDiv=document.createElement("div");
-        taskDiv.classList.add("task-item");
-        // li yaratma
-        let taskLi=document.createElement("li");
-        taskLi.classList.add("task-tanim")
-        taskLi.innerText=newTask.value;
-        taskDiv.appendChild(taskLi)
-        taskList.appendChild(taskDiv)
-        // button yaratma
-        let taskBtnTamam=document.createElement("button");
-        taskBtnTamam.classList.add("task-btn");
-        taskBtnTamam.classList.add("task-btn-tamam");
-        taskBtnTamam.innerHTML='<i class="fa-solid fa-square-check"></i>';
-        taskDiv.appendChild(taskBtnTamam);
-       
-        let taskBtnSil=document.createElement("button");
-        taskBtnSil.classList.add("task-btn");
-        taskBtnSil.classList.add("task-btn-sil");
-        taskBtnSil.innerHTML='<i class="fa-solid fa-trash"></i>';
-        taskDiv.appendChild(taskBtnSil);
 
-        // LocalStorage
-        logalStoage(newTask.value);
-        newTask.value='';
-    }
+
+function taskAdd(e){
+    taskYarat(newTask.value)
+    logalStoage(newTask.value);
+    newTask.value='';
+    
     e.preventDefault()
     
 }
-
 taskList.addEventListener("click",tasksilTamamla)
-
 function tasksilTamamla(e){
     let klikElementi=e.target
     if(klikElementi.classList.contains("task-btn-tamam")){
@@ -47,14 +22,15 @@ function tasksilTamamla(e){
 
     if(klikElementi.classList.contains("task-btn-sil")){
         klikElementi.parentElement.classList.toggle("yoxacix")
+        let silinenElement=klikElementi.parentElement.children[0].innerText
+        console.log(silinenElement)
+        localStorageSil(silinenElement)
         klikElementi.parentElement.addEventListener("transitionend",function(){
                klikElementi.parentElement.remove()
         })
     }
 
 }
-
-
 function logalStoage(newTask){
 let tasklar;
 if(localStorage.getItem('tasklar')===null){
@@ -65,8 +41,52 @@ if(localStorage.getItem('tasklar')===null){
 tasklar.push(newTask);
 localStorage.setItem('tasklar', JSON.stringify(tasklar));
 }
+function localStorageOxu(e){
+    e.preventDefault();
 
+    let tasklar;
+if(localStorage.getItem('tasklar')===null){
+    tasklar=[];
+}else{
+    tasklar=JSON.parse(localStorage.getItem('tasklar'));
+}
+tasklar.forEach(task => {
+    taskYarat(task)
+});
+    
+}
+function taskYarat(task){
+    let taskDiv=document.createElement("div");
+    taskDiv.classList.add("task-item");
+    // li yaratma
+    let taskLi=document.createElement("li");
+    taskLi.classList.add("task-tanim")
+    taskLi.innerText=task;
+    taskDiv.appendChild(taskLi)
+    taskList.appendChild(taskDiv)
+    // button yaratma
+    let taskBtnTamam=document.createElement("button");
+    taskBtnTamam.classList.add("task-btn");
+    taskBtnTamam.classList.add("task-btn-tamam");
+    taskBtnTamam.innerHTML='<i class="fa-solid fa-square-check"></i>';
+    taskDiv.appendChild(taskBtnTamam);
+   
+    let taskBtnSil=document.createElement("button");
+    taskBtnSil.classList.add("task-btn");
+    taskBtnSil.classList.add("task-btn-sil");
+    taskBtnSil.innerHTML='<i class="fa-solid fa-trash"></i>';
+    taskDiv.appendChild(taskBtnSil);
+}
 
-function localStorageOxu(){
-
+function localStorageSil(task){
+    let tasklar;
+if(localStorage.getItem('tasklar')===null){
+    tasklar=[];
+}else{
+    tasklar=JSON.parse(localStorage.getItem('tasklar'));
+}
+    // splice ile elementi sil
+    let silinenElementinindexi=tasklar.indexOf(task)
+    console.log(silinenElementinindexi)
+    tasklar.splice(silinenElementinindexi,1)
 }
