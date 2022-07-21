432da6277093a03389f95a8daf47f455bb5ff28c
GulsenZalova <zalova0202@gmail.com>
2022-07-22 01:14:27 +0400

    local storage

--- Javascipt API/Web Stroage/app.js
+++ Javascipt API/Web Stroage/app.js
@@ -1,44 +1,52 @@
+let additem=document.querySelector("#addItem")
+let deleteitem=document.querySelector("#deleteItem")
+let contentText=document.querySelector(".content")
+let input=document.querySelector(".input")
+let items;
 
-let textBox=document.getElementById("text-box");
-let btnadd=document.getElementById("add");
-let btndelete=document.getElementById("delete");
-let content=document.getElementById("content");
-let messagges=[]
+additem.addEventListener("click" , addLocalStorage)
+window.addEventListener("load",contentTextFunc)
+deleteitem.addEventListener("click",deleteLocalStorage)
 
-btnadd.addEventListener("click",addİnfo)
-window.addEventListener("load", contentText)
-btndelete.addEventListener("click",infoDelete)
 
-function addİnfo(){
-   if(textBox.value !==''){
-    messagges.push(textBox.value)
-    localStorage.setItem("messagge", JSON.stringify(messagges))
-    contentText()
-    textBox.value=""
-   }
-   else{
-       alert("Məlumat daxil etmədiniz")
-   }
+function addLocalStorage(e){
+    e.preventDefault()
+    if(input.value===""){
+        alert("Siz Məlumat daxil etməmisiniz!!!")
+    }
+    else{
+       if(localStorage.getItem("addInfo")===null){
+            items=[]
+       }else{
+        JSON.parse(localStorage.getItem("addInfo"))
+       }
+       items.push(input.value)
+       localStorage.setItem("addInfo",JSON.stringify(items))
+       contentTextFunc()
+       input.value=""
+    }
+
 }
 
-function contentText(){
-    if(localStorage.getItem("messagge")===null){
-        content.innerText="Hec bir məlumat tapılmadı."
+function contentTextFunc(){
+    if(localStorage.getItem("addInfo")===null){
+        contentText.innerText="Local Storage-də göstəriləcək bir məlumat yoxdur"
     }else{
-        messagges=JSON.parse(localStorage.getItem("messagge"))
-        console.log(messagges[0])
-        content.innerText=''
-        for(let messagge in messagges){
-            content.innerHTML+=`${messagges[messagge]}<br>`
-        }
+     contentText.innerText=""  
+     let infos=JSON.parse(localStorage.getItem("addInfo"))
+     infos.forEach(info => {
+        contentText.innerHTML+=`${info}<br>`
+     });
     }
 }
 
-function infoDelete(){
-    if(localStorage.getItem("messagge")===null){
-        alert("Burda silinəcək bir məlumat yoxdur")
-    }else{
-        localStorage.removeItem("messagge")
-        contentText()
+function deleteLocalStorage(){
+    if(localStorage.getItem("addInfo")===null){
+        alert("LocalStorage-də silinəcək bir məlumat yoxdur")
+    }
+    else{
+        localStorage.removeItem("addInfo")
+        contentTextFunc()
     }
 }
+
